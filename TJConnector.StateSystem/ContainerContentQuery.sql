c1ec3fd28cf6f801398c46d6327cca3939e7e151
razleytsev <sergey_razleytsev@rikitlab.com>
2025-02-02 20:32:55 +0300

    add files back

---
+++ TJConnector.StateSystem/ContainerContentQuery.sql
@@ -0,0 +1,13 @@
+WITH CH AS
+(SELECT Id, Code, REPLACE(FullCode,']','') AS FullCode, ParentId FROM intobj.ConsolidatedHierarchiesArchive
+UNION ALL 
+SELECT Id, Code, REPLACE(FullCode,']','') AS FullCode, ParentId FROM intobj.ConsolidatedHierarchies),
+CI AS
+(SELECT Id, Code, REPLACE(FullCode,']','') AS FullCode, ParentId FROM intobj.ConsolidatedItemsArchive
+UNION ALL 
+SELECT Id, Code, REPLACE(FullCode,']','') AS FullCode, ParentId FROM intobj.ConsolidatedItems)
+
+SELECT Bundles.FullCode AS Bundle, Packs.FullCode AS Pack FROM CH AS MC
+INNER JOIN CH AS Bundles ON Bundles.ParentId = MC.Id 
+INNER JOIN CI AS Packs ON Packs.ParentId = Bundles.Id
+WHERE MC.Code = @Code
